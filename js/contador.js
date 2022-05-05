80913d8443d3957c806e89b9237018a26bb417e5
Alejandro Espinosa <alejandroriveros2007@hotmail.com>
2022-05-05 09:02:11 -0500

    boton publicador y contador
    Se hizo mas ajustes al contador regresivo y se agrego el boton el cual sera el publicador

--- js/contador.js
+++ js/contador.js
@@ -3,13 +3,13 @@ simplyCountdown("#cuenta", {
     month: 5, // required
     day: 5, // required
     hours: 8, // Default is 0 [0-23] integer
-    minutes: 45, // Default is 0 [0-59] integer
+    minutes: 55, // Default is 0 [0-59] integer
     seconds: 0, // Default is 0 [0-59] integer
     words: { //words displayed into the countdown
-        days: { singular: 'day', plural: 'days' },
-        hours: { singular: 'hour', plural: 'hours' },
-        minutes: { singular: 'minute', plural: 'minutes' },
-        seconds: { singular: 'second', plural: 'seconds' }
+        days: { singular: 'DIA', plural: 'DIAS' },
+        hours: { singular: 'HORA', plural: 'HORAS' },
+        minutes: { singular: 'MINUTO', plural: 'MINUTOS' },
+        seconds: { singular: 'SEGUNDO', plural: 'SEGUNDOS' }
     },
     plural: true, //use plurals
     inline: false, //set to true to get an inline basic countdown like : 24 days, 4 hours, 2 minutes, 5 seconds
