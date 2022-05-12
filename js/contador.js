70c3f23c7448a36111480a529837ee1382c26f6f
Alejo207 <alejandroriveros2007@hotmail.com>
2022-05-11 20:23:27 -0500

    v1

--- js/contador.js
+++ js/contador.js
@@ -1,9 +1,9 @@
 simplyCountdown("#cuenta", {
     year: 2022, // required
     month: 5, // required
-    day: 8, // required
-    hours: 8, // Default is 0 [0-23] integer
-    minutes: 36, // Default is 0 [0-59] integer
+    day: 11, // required
+    hours: 19, // Default is 0 [0-23] integer
+    minutes: 57, // Default is 0 [0-59] integer
     seconds: 0, // Default is 0 [0-59] integer
     words: { //words displayed into the countdown
         days: { singular: 'DIA', plural: 'DIAS' },
@@ -18,7 +18,9 @@ simplyCountdown("#cuenta", {
     enableUtc: false, //Use UTC or not - default : false
     onEnd: function() { 
         /*alert("NUEVA NOTICIA!!!!!!🥳"),*/
-        document.getElementById("low").classList.add('oculto');
+        document.getElementById("low").classList.add('borrar');
+        var p= document.getElementById("xd");
+        p.removeChild("low");
         return; }, //Callback on countdown end, put your own function here
     refresh: 1000, // default refresh every 1s
     sectionClass: 'simply-section', //section css class
