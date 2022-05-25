46c2034ca8cada6b0b1ca14b2a533ea4e0eca267
Alejandro Espinosa <alejandroriveros2007@hotmail.com>
2022-05-25 08:13:16 -0500

    ,

--- js/contador.js
+++ js/contador.js
@@ -2,7 +2,7 @@ simplyCountdown("#cuenta", {
     year: 2022, // required
     month: 5, // required
     day: 25, // required
-    hours: 8, // Default is 0 [0-23] integer
+    hours: 12, // Default is 0 [0-23] integer
     minutes: 0, // Default is 0 [0-59] integer
     seconds: 0, // Default is 0 [0-59] integer
     words: { //words displayed into the countdown
